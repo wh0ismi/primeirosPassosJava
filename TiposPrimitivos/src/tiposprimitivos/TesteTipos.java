5889645c6d157395d5322f24ff5036b20556f85f
wh0ismi <57287249+wh0ismi@users.noreply.github.com>
2021-05-07 09:54:32 -0300

    Tipos primitivos e manipulação de dados.

---
+++ TiposPrimitivos/src/tiposprimitivos/TesteTipos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author miria
+ */
+public class TesteTipos {
+
+    public static void main(String[] args) {
+       /*
+        * int idade = 30;
+        * String valor = Integer.toString(idade);
+        * System.out.println(valor);
+        
+        * String valor = "30";
+        * int idade = Integer.parseInt(valor);
+        * System.out.println(idade);
+        */
+    }
+}
